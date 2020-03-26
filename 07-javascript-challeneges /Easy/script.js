3335ce75a1cbe6ea5765072f387ae2bf547e26c4
alan vang <vangnalan@gmail.com>
2020-03-25 21:03:04 -0400

    added 07-Javascript Challenges

---
+++ 07-javascript-challeneges /Easy/script.js
@@ -0,0 +1,29 @@
+// EASY: ​
+// Create an array of students holding their last name, 
+// first name, 
+// and age with 
+// 3 students. 
+// To validate, please log a greeting with the 
+// first name, last name and age of the 2nd student. 
+// The output should look like "Hello, my name is John Doe and I'm 19 years old."
+
+
+var student1 = ['Jones', 'Bobby', '28'];
+var student2 = ['Wick', 'John', '30'];
+var student3 = ['Ketchup', 'Ash', '25'];
+
+console.log(`Hello, my name is ${student1[1]} ${student1[0]} and i'm ${student1[2]} years old.`);
+console.log(`Hello, my name is ${student2[1]} ${student2[0]} and i'm ${student2[2]} years old.`);
+console.log(`Hello, my name is ${student3[1]} ${student3[0]} and i'm ${student3[2]} years old.`);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 07-javascript-challeneges /Very easy/script.js
@@ -0,0 +1,16 @@
+// VERY EASY: 
+// Write a function named min that takes two arguments 
+// and returns their minimum.
+
+function min(num1, num2){
+   return Math.min(num1, num2);
+}
+
+min(5,10);
+
+console.log(min(5,10));
+
+
+
+
+
