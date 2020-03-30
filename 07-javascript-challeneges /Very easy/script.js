18be065036a4fbed31db8ccd1cd80d4d6dd4b6e7
alan vang <vangnalan@gmail.com>
2020-03-30 11:10:51 -0400

    added Checkpoint array

--- 07-javascript-challeneges /Very easy/script.js
+++ 07-javascript-challeneges /Very easy/script.js
@@ -11,6 +11,26 @@ min(5,10);
 console.log(min(5,10));
 
 
+//  In class challenge (March 26th, 2020.)
+// var podNames = ['Adrian', 'Adrianna', 'Alan\s', 'Ana', 'Zell', 'Caleb'];
+// podNames.push('Tori');
+// console.log(podNames);
+
+// if(podNames.indexOf('Tori') === -1){
+//    console.log(`Tori is not present. R2H pods are awesome.`)
+// } else {
+//       podNames.pop();
+//       console.log(`Here is the current array ${podNames}`)
+//    }
+
+// console.log(`There are ${podNames.length - 1} people in ${podNames[2]} pod.`); // my prefered method *new
+
+// console.log('There are ' + (podNames.length - 1) + ' people in ' + (podNames[2] + ' pod.')) // old method. 
+
+
+
+
+
 
 
 
---
+++ Checkpoint Arrays/index.js
@@ -0,0 +1,9 @@
+var drinks = [];
+drinks [water, coffe, tea];
+
+var foods = [];
+foods [apple, sandwhich, chips];
+
+var meals = [drinks, foods];
+
+console.log(meals);
